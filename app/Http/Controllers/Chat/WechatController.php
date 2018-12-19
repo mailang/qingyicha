41fcaa5272742c93837e18c28f1a82b0fa6d533a
cpcode <32060310+cpcode@users.noreply.github.com>
2018-12-19 11:04:11 +0800

    update

--- app/Http/Controllers/Chat/WechatController.php
+++ app/Http/Controllers/Chat/WechatController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use EasyWeChat\Factory;
 use EasyWeChat\Work\Application;
+use Illuminate\Support\Facades\Log;
 
 class WechatController extends Controller
 {
@@ -19,10 +20,12 @@ class WechatController extends Controller
        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
+
         $app->server->push(function($message){
             return "欢迎关注 overtrue！";
         });
 
         return $app->server->serve();
+
     }
 }
