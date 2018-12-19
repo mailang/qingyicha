88077a1aca42d48d1c35ea2e4fb026dc84d79761
cpcode <32060310+cpcode@users.noreply.github.com>
2018-12-19 15:28:42 +0800

    1

--- app/Http/Controllers/Chat/WechatController.php
+++ app/Http/Controllers/Chat/WechatController.php
@@ -28,28 +28,4 @@ class WechatController extends Controller
         return $app->server->serve();
 
     }
-    private function checkSignature()
-    {
-        // you must define TOKEN by yourself
-        if (!defined("TOKEN")) {
-            throw new Exception('TOKEN is not defined!');
-        }
-
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        // use SORT_STRING rule
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-    }
 }
