84e9c9ad9dd3618ef5a691cd080b159d6ddbf576
cpcode <32060310+cpcode@users.noreply.github.com>
2019-01-23 16:41:11 +0800

    22

--- app/Http/Controllers/Chat/PayController.php
+++ app/Http/Controllers/Chat/PayController.php
@@ -61,38 +61,36 @@ class PayController extends Controller
         $data1["result"]="进入到了回调地址里";
         \DB::table('record')->insert($data1);
 
-        $app = app('wechat.official_account');
-        $response = $app->handlePaidNotify(function($message, $fail){
+        $app = app('wechat.payment');
+        $payment=$app->payment;
+        $response =$payment->handlePaidNotify(function($message, $fail){
             Log::info(var_export($message));
             // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
             $data['openid']="offTY1fb81WxhV84LWciHzn4qwqU";
             $data["result"]=var_export($message);
             \DB::table('record')->insert($data);
-            $order = $message['out_trade_no'];
-            if($order || $order->paid_at) { // 如果订单不存在 或者 订单已经支付过了
-              return true; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
-    }
-     //<- 建议在这里调用微信的【订单查询】接口查一下该笔订单的情况，确认是已经支付 /////////////
-      if($message['return_code'] === 'SUCCESS') { // return_code 表示通信状态，不代表支付状态
-         // 用户是否支付成功
-        if (array_get($message, 'result_code') === 'SUCCESS') {
-            $order->paid_at = time(); // 更新支付时间为当前时间
-            $order->status = 'paid';
-            // 用户支付失败
-        } elseif (array_get($message, 'result_code') === 'FAIL') {
-            $order->status = 'paid_fail';
-        }
-      } else {
+            //<- 建议在这里调用微信的【订单查询】接口查一下该笔订单的情况，确认是已经支付 /////////////
+            if(strtolower($message['return_code']) === 'success') { // return_code 表示通信状态，不代表支付状态
+                $order = Order::where('out_trade_no',$message['out_trade_no'])->first();
+                if ($order )
+                {
+                    // 用户是否支付成功
+                    if (array_get($message, 'result_code') === 'SUCCESS') {
+                        //$order->paid_at = time(); // 更新支付时间为当前时间
+                        $order["state"]=1;
+                        // 用户支付失败
+                    } elseif (array_get($message, 'result_code') === 'FAIL') {
+                        $order["state"]=-2;
+                    }
+                }
+                else
+                    return $fail('订单号不存在');
+            } else {
                 return $fail('通信失败，请稍后再通知我');
-    }
-
-    $order->save(); // 保存订单
-
-    return true; // 返回处理完成
+            }
+              return true; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
     });
-
     $response->send(); // return $response;
-
     }
 
     /*微信退款*/
--- database/migrations/2019_01_11_071008_order.php
+++ database/migrations/2019_01_11_071008_order.php
@@ -22,7 +22,7 @@ class Order extends Migration
             $table->string('body')->nullable();//商品简单描述
             $table->float('total_fee')->nullable();//订单总金额
             $table->float('actual_fee')->nullable();//实际支付金额
-            //  订单状态 0:未支付1：已付款，2：征信接口已成功查询；3.接口已查询存在异常接口-1：超时未支付的无效订单
+            //  订单状态 0:未支付1：已付款，2：征信接口已成功查询；3.接口已查询存在异常接口-1：超时未支付的无效订单-2:支付失败
             $table->integer('state')->default(0);
             $table->string ('time_start')->nullable();//格式20091225091010
             $table->string ('time_expire')->nullable();//格式20091225091010
