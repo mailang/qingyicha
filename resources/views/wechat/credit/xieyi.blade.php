c1d0f28712bc20837d6ada967ee378eb4899b42f
cpcode <32060310+cpcode@users.noreply.github.com>
2019-01-23 13:31:51 +0800

    2

--- resources/views/wechat/credit/xieyi.blade.php
+++ resources/views/wechat/credit/xieyi.blade.php
@@ -55,38 +55,38 @@
     $(function () {
         wx.config(<?php echo app('wechat.official_account')->jssdk->buildConfig(array('chooseWXPay'), true) ?>);
         wx.ready(function(){
-          alert("成功");
+            // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+            $("#SubmitBtn").click(function () {
+                $.ajax({
+                    url: '{{route('order.create',$product->id)}}',
+                    type: 'get',
+                    datatype: 'json',
+                    success: function (data) {
+                        $re=$.parseJSON(data);
+                        wx.chooseWXPay({
+                            timestamp:$re["timestamp"],
+                            nonceStr: $re["nonceStr"],
+                            package: $re["package"],
+                            signType:$re["signType"],
+                            paySign: $re["paySign"], // 支付签名
+                            success: function (res) {
+                                //支付成功后的回调函数
+                                //支付成功后生成征信报告
+                                weui.toast('支付回调');
+                            }
+                        });
+                    },
+                    error: function () {
+                        weui.toast('系统故障');
+                    }
+                });
+            });
         });
         wx.error(function(res){
             // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
         alert(res);
         });
-        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-        $("#SubmitBtn").click(function () {
-            $.ajax({
-                url: '{{route('order.create',$product->id)}}',
-                type: 'get',
-                datatype: 'json',
-                success: function (data) {
-                    $re=$.parseJSON(data);
-                    wx.chooseWXPay({
-                        timestamp:$re["timestamp"],
-                        nonceStr: $re["nonceStr"],
-                        package: $re["package"],
-                        signType:$re["signType"],
-                        paySign: $re["paySign"], // 支付签名
-                        success: function (res) {
-                            //支付成功后的回调函数
-                            //支付成功后生成征信报告
-                            weui.toast('支付回调');
-                        }
-                    });
-                },
-                error: function () {
-                    weui.toast('系统故障');
-                }
-            });
-        });
+
     });
 
 </script>
