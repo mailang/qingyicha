b2ae9a46f1d5a866448cce9220670999e2b8d731
cpcode <32060310+cpcode@users.noreply.github.com>
2019-01-24 14:21:53 +0800

    q

--- database/migrations/2019_01_11_071030_order_result.php
+++ database/migrations/2019_01_11_071030_order_result.php
@@ -15,20 +15,13 @@ class OrderResult extends Migration
     {
         Schema::create('order_result', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('wxuser_id');
-            $table->integer('auth_id');
             $table->string('openid');
+            $table->integer('order_id');
             $table->string('out_trade_no')->nullable();//订单号
             $table->string('transaction_id')->nullable();//微信订单号
             $table->string('return_code')->nullable();//返回状态值SUCCESS/FAIL
-            $table->string('return_msg')->nullable();//return_code为FAIL时的返回值
-            $table->string('nonce_str')->nullable();
-            $table->string('sign')->nullable();
-            $table->string('result_code')->nullable();//业务结果SUCCESS/FAIL
-            $table->string('err_code')->nullable();//FAIL时的返回值
-            $table->string('trade_type')->nullable();
-            /*微信生成的预支付会话标识，用于后续接口调用中使用，该值有效期为2小时*/
-            $table->string('prepay_id')->nullable();
+            $table->string('result_code')->nullable();//return_code为FAIL时的返回值
+            $table->text('result')->nullable();//微信端返回xml记录
             $table->timestamps();
 
         });
