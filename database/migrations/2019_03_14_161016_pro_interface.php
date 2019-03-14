8b38b2bea9bb1d8d00d1121e306a5197c2d3c518
cpcode <32060310+cpcode@users.noreply.github.com>
2019-03-14 17:24:55 +0800

    888

--- database/migrations/2019_03_14_161016_pro_interface.php
+++ database/migrations/2019_03_14_161016_pro_interface.php
@@ -16,8 +16,8 @@ class ProInterface extends Migration
         Schema::create('pro_interface', function (Blueprint $table) {
             $table->increments('id');
             /*接口id*/
-            $table->integer('interface_id');
-            $table->integer('pro_id');
+            $table->integer('interface_id')->unsigned();
+            $table->integer('pro_id')->unsigned();
             $table->integer('isenable')->default(0);
             $table->timestamps();
             $table->foreign('interface_id')->references('id')->on('interfaces')
